b0ebcad84e2f4d4f723f99cfcdf1fbed6e085d91
tonpai <tara@kkumail.com>
2016-11-12 16:45:22 +0700

    Finished

--- app/src/main/java/kku/tonpai/easykku/SignUpActivity.java
+++ app/src/main/java/kku/tonpai/easykku/SignUpActivity.java
@@ -1,8 +1,14 @@
 package kku.tonpai.easykku;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
+import android.os.StrictMode;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
@@ -12,6 +18,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import org.jibble.simpleftp.SimpleFTP;
+
+import java.io.File;
+
 public class SignUpActivity extends AppCompatActivity {
 
 
@@ -19,7 +29,10 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText nameEditText, phoneEditText, userEditText,passwordEditText;
     private ImageView imageView;
     private Button button;
-    private String nameString, phoneString,userString,passwordString;
+    private String nameString, phoneString,userString,passwordString, imagePathString, imageNameString;
+    private Uri uri;
+    private boolean aBoolean = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +45,7 @@ public class SignUpActivity extends AppCompatActivity {
         passwordEditText =(EditText)findViewById(R.id.editText2);
         imageView = (ImageView) findViewById(R.id.imageView);
         button = (Button) findViewById(R.id.button3);
+
 
         //SignUp Controller
         button.setOnClickListener(new View.OnClickListener() {
@@ -52,6 +66,13 @@ public class SignUpActivity extends AppCompatActivity {
                     MyAlert myAlert = new MyAlert(SignUpActivity.this,R.drawable.doremon48,
                             "มีช่องว่าง","กรุณากรอกให้ครบทุกช่องค่ะ");
                     myAlert.myDialog();
+                } else if (aBoolean) {
+                    //Non Choose Image
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.nobita48,"ยังไม่เลือกรูป",
+                    "กรุณาเลือกรูปด้วยค่ะ");
+                } else {
+                    //Chose Image OK
+                    upLoadImageToServer();
                 }
 
             }//onClick
@@ -70,6 +91,25 @@ public class SignUpActivity extends AppCompatActivity {
 
     }//Main Method
 
+    private void upLoadImageToServer() {
+        //Change Policy
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try{
+            SimpleFTP simpleFTP = new SimpleFTP();
+            simpleFTP.connect("ftp.swiftcodingthai.com",21,"kku@swiftcodingthai.com","Abc12345");
+            simpleFTP.bin();
+            simpleFTP.cwd("Image");
+            simpleFTP.stor(new File(imagePathString));
+            simpleFTP.disconnect();
+        }catch (Exception e){
+            Log.d("12NovV1", "e simple FTP==> "+e.toString());
+        }
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -77,8 +117,43 @@ public class SignUpActivity extends AppCompatActivity {
         if ( (requestCode == 0) && (resultCode == RESULT_OK) ) {
 
             Log.d("12novV1", "Result OK");
+            aBoolean = false;
+            uri = data.getData();
+            try{
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            //Find Path of Image
+            imagePathString = myFindPath(uri);
+            Log.d("12NovV1", "ImagePath ==> "+imagePathString);
+
+            //Find Name of Image
+            /*imageNameString = imagePathString.substring(imagePathString.lastIndexOf("/"));
+            Log.d("12NovV1","imageName==>" +imageNameString);*/
 
         } // if
 
     }//onActivity
+
+    private String myFindPath(Uri uri) {
+
+        String result = null;
+        String[] strings = {MediaStore.Images.Media.DATA};
+        //การดึงฐานข้อมูลไปทำงานอยู่ใน RAM
+        Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
+
+        if (cursor!= null) {
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            result = cursor.getString(index);
+        } else {
+            result = uri.getPath();
+        }
+
+        return null;
+    }
 }//Main Class
